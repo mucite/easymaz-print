d065221c112ac1db3a9be153dd4692a5323bac8a
mucite <musie_kebede@icloud.com>
2025-08-26 13:48:39 +0200

    fix log

--- src/print.js
+++ src/print.js
@@ -7,8 +7,9 @@ const winston = require('winston');
 const logger = winston.createLogger({
   level: 'info',
   transports: [
-    new winston.transports.Console({ format: winston.format.simple() }),
-    new winston.transports.File({ filename: '../app.log', level: 'error' })
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
   ]
 });
 
