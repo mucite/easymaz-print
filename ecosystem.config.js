7fd9b65ae7f47d52af35dc6130f3f4b294062078
mucite <musie_kebede@icloud.com>
2025-08-29 00:10:01 +0200

    fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,8 @@ module.exports = {
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
-        API_URL: 'http://localhost:8081'
+        API_URL: 'http://localhost:8081',
+        RESTAURANT_ID: '68ac8a5f066bb634561fd848'
       },
     },
   ],
